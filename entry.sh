8801079a1ff45b70468b4b644c36df9412e22bf7
HackedServer <hs@hackedserver.net>
2021-11-08 00:44:01 +0000

    working on entry.sh

--- entry.sh
+++ entry.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python3 -m pipenv install && python3 -m pipenv run python3 hs-plugins/
+python3 -m pip install pipenv
+python3 -m pipenv install
+python3 -m pipenv run python3 hs-plugins/
